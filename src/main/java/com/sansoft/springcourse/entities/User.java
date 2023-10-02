dccadc21e82e5254034ce37484d2236fb0fcd038
Thiago S. Melo <thiagosantiagomelo@gmail.com>
2023-10-02 00:32:36 -0300

    H2 database, test profile, JPA

--- src/main/java/com/sansoft/springcourse/entities/User.java
+++ src/main/java/com/sansoft/springcourse/entities/User.java
@@ -1,5 +1,6 @@
 package com.sansoft.springcourse.entities;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.io.Serial;
@@ -7,12 +8,16 @@ import java.io.Serializable;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "tb_user")
 @NoArgsConstructor
 @AllArgsConstructor
 public class User implements Serializable {
     @Serial private static final long serialVersionUID = 1L;
 
+    @Id
     @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
