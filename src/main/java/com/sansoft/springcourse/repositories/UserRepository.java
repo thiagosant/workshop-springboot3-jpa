e1125e05a041354af7358546312c0ec476d4c511
Thiago S. Melo <thiagosantiagomelo@gmail.com>
2023-10-03 22:18:31 -0300

    Service layer, component registration

--- src/main/java/com/sansoft/springcourse/repositories/UserRepository.java
+++ src/main/java/com/sansoft/springcourse/repositories/UserRepository.java
@@ -2,6 +2,7 @@ package com.sansoft.springcourse.repositories;
 
 import com.sansoft.springcourse.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
