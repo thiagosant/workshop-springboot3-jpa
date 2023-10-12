e791035ecec04f866f293a388818b0c4eaa6fb55
Thiago S. Melo <thiagosantiagomelo@gmail.com>
2023-10-12 20:01:06 -0300

    Payment, one-to-one association

--- src/main/java/com/sansoft/springcourse/entities/Order.java
+++ src/main/java/com/sansoft/springcourse/entities/Order.java
@@ -41,6 +41,9 @@ public class Order implements Serializable {
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
 
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
     public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
