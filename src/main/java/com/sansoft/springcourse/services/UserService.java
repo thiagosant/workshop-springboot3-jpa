3cec277ab7e61ec411ecf6fad913ca4fdf440576
Thiago S. Melo <thiagosantiagomelo@gmail.com>
2023-10-12 21:16:12 -0300

    Exception handling - delete

--- src/main/java/com/sansoft/springcourse/services/UserService.java
+++ src/main/java/com/sansoft/springcourse/services/UserService.java
@@ -2,8 +2,11 @@ package com.sansoft.springcourse.services;
 
 import com.sansoft.springcourse.entities.User;
 import com.sansoft.springcourse.repositories.UserRepository;
+import com.sansoft.springcourse.services.exceptions.DatabaseException;
 import com.sansoft.springcourse.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,16 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        }
+        catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        }
+        catch (DataIntegrityViolationException e ) {
+            throw new DatabaseException(e.getMessage());
+        }
+
     }
 
     public User update(Long id, User obj) {
